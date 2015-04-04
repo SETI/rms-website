812fd259616f967d180ad5e419af8e5fb19e1946
Lisa Ballard <lballard.cat@gmail.com>
2015-04-03 23:07:09 -0700

    first stab at scraping html into markdown

--- scrapers.py
+++ scrapers.py
@@ -3,51 +3,70 @@ import re
 import html2text  # https://github.com/aaronsw/html2text
 # from bs4 import BeautifulSoup
 
-base_path_original = "/Users/lballard/projects/rn_scrape/original/rn_site/"
-base_path_new = "/Users/lballard/projects/rn_scrape/new/rn_site/"
+base_path_original = "/Users/lballard/projects/rn_scrape/original-site/"
+base_path_new = "/Users/lballard/projects/rn_scrape/new/"
 
 exclude_contains = ['_v[0-9]', '~$', '.css']
 
 def spider_site(base_path_original, file_type):
     """ spider site to find directory structure and files """
 
+    print base_path_original
     for root, dirs, files in os.walk(base_path_original):
         path = '/'.join(root.split('/')) + '/'
         # print (len(path) - 1) *'---' , os.path.basename(root)       
-        print '--------------------'
-        print path + os.path.basename(root)       
-        print '--------------------'
         for f in files:
-            print f
             
             if [re.search(ex, f) for ex in exclude_contains if re.search(ex, f)]:
-                continue
+                continue  # these are old versions and other things to exclude
 
             if 'html' in f:
-                # print len(path)*'---', f
-                print path + f
+                # this is an html file so try to convert it and put in new place
+
+                full_path =  path + f
+                new_path = full_path.replace('original-site','new')
 
                 try: 
-                    md = convert_page(path + f)
-
-                    if 'PDS_VERSION_ID' in md:
-                        continue
-
-                    """
-                        todo: 
-                        figure out where to put this markdown in the /new structure
-                        create directories if needed
-                        keep figures and images along side? 
-                    """
-
-                    print md
+                    content = convert_page(full_path)
+                    if 'PDS_VERSION_ID' in content:
+                        continue  # these are PDS indexes named like html files
 
                 except UnicodeDecodeError:
-                    """
-                        todo:
-                        these html files could not be converted, copy them over as they are 
-                    """
-                    print 'NOPE'
+                    # these html files could not be converted, copy them over as they are 
+                    with open (full_path, "r") as myfile:
+                        content = myfile.read()
+
+                create_file(new_path, content)
+
+
+
+
+def create_file(filepath, content):
+
+    file_split = [ a for a in filepath.split('/') if a.strip() ]
+
+    for k, file_part in enumerate(file_split):
+        
+        # print file_part 
+
+        if k < len(file_split)-1:
+            this_dir = '/' + '/'.join(file_split[0: file_split.index(file_part) + 1])
+
+            if not this_dir:
+                continue
+
+            if not os.path.isdir(this_dir):
+                os.makedirs(this_dir)
+                print "creating directory" + this_dir
+
+        else:
+            # write content to file_path
+            text_file = open(filepath, "w")
+            text_file.write(content)
+            text_file.close()
+
+
+
 
 
 
@@ -67,8 +86,8 @@ def convert_page(filename):
 		"""
 
 
-
-
+if __name__ == "__main__":
+    spider_site(base_path_original, 'html')
 
 
 
