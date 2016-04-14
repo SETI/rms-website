9d43d3aa0bf68e368e8f38ad329bedb340474811
Lisa Ballard <lballard.cat@gmail.com>
2016-04-14 16:59:48 -0700

    makes deploy script do a dry run first, shows changed files

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -18,13 +18,22 @@ def deploy():
     # Note that _config.yml must not be excluding any files
     with lcd("../website"):
         local("jekyll build")
-        local("rsync -rv --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ %s@pds-rings.seti.org:~/website/." % (PROD_USR))
+        print('Transfering to your remote home directory..')
+        local("rsync -r --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ %s@pds-rings.seti.org:~/website/." % (PROD_USR))
 
     # on the server, copy the website to the production directory
+    rsync_cmd = "sudo rsync -r %s --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz website/ %s. " 
+
+    # first do a dry run:
     with cd('~/'):
-        run("sudo rsync -rv --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz website/ %s/. " % (PROD_DIR))
+        run(rsync_cmd % ('--dry-run --itemize-changes ',PROD_DIR))
 
+    if confirm("The above was a dry run. If the above looks good, push to production:"):
+        with cd('~/'):
+            run(rsync_cmd % ('',PROD_DIR))
 
-    print("Finished! \n")
-    sys.exit()
-    
+        print("Finished! \n")
+        sys.exit()
+
+    else:
+        print("Deployment Aborted")
