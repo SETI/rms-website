0763430279486dd9c67d200ee512b758e50ccae0
Lisa Ballard <lballard.cat@gmail.com>
2016-04-29 11:51:39 -0700

    makes deploy script work on production server too

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import sys
+import socket
 from fabric.api import env, cd, lcd, run, local, abort
 from fabric.contrib.console import confirm
 from secrets import PROD_DIR, PROD_USR
@@ -10,30 +11,53 @@ def deploy():
     """ this will pull the website from github, build it locally, transfer
         it to production server, and move it into production directory on that server"""
 
+    # is this your laptop or logged into the web server? (want this script to work on both)
+    web_server = False
+    if 'pds-rings' in socket.gethostname():
+        web_server = True
+
     # You are in /deploy so go a level up and git pull
     with lcd("../"):
+        print("pulling the latest site from github: https://github.com/basilleaf/ringsnode_website")
         local("git pull")
 
     # build the website. 
-    # Note that _config.yml must not be excluding any files
+    # Note that _config.yml must not be excluding any files!!!! 
+    # TODO: make this impossible to fail
     with lcd("../website"):
+        
         local("jekyll build")
-        print('Transfering to your remote home directory..')
-        local("rsync -r --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ %s@pds-rings.seti.org:~/website/." % (PROD_USR))
+
+        if web_server:
+            # we are on the web server, 
+            # transfer _site to a local directory called "website" 
+            local("rsync -r --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ ~/website/.")
+        else: 
+            # copy the local _site to the remote directory on pds-rings called "website"
+            print('Transfering to your remote home directory..')
+            local("rsync -r --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ %s@pds-rings.seti.org:~/website/." % (PROD_USR))
 
     # on the server, copy the website to the production directory
     rsync_cmd = "sudo rsync -r %s --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz website/ %s. " 
-
-    # first do a dry run:
-    with cd('~/'):
-        run(rsync_cmd % ('--dry-run --itemize-changes ',PROD_DIR))
-
-    if confirm("The above was a dry run. If the above looks good, push to production:"):
+    if web_server: 
+        with lcd('~/'):
+            # first do a dry run:
+            local(rsync_cmd % ('--dry-run --itemize-changes ',PROD_DIR))
+            if confirm("The above was a dry run. If the above looks good, push to production:"):
+                local(rsync_cmd % ('',PROD_DIR))
+                print("\n*** Production Website Has Been Updated! ***\n")
+                sys.exit()
+            else:
+                print("\nDeployment Aborted\n")        
+    else: 
         with cd('~/'):
-            run(rsync_cmd % ('',PROD_DIR))
+            # first do a dry run:
+            run(rsync_cmd % ('--dry-run --itemize-changes ',PROD_DIR))
+            if confirm("The above was a dry run. If the above looks good, push to production:"):
+                run(rsync_cmd % ('',PROD_DIR))
+                print("\n*** Production Website Has Been Updated! ***\n")
+                sys.exit()
+            else:
+                print("\nDeployment Aborted\n")        
 
-        print("Finished! \n")
-        sys.exit()
 
-    else:
-        print("Deployment Aborted")
