0180c0133d1cb6a1f0d9c406a129d85baa9652f5
Lisa Ballard <lballard.cat@gmail.com>
2016-08-17 12:42:14 -0700

    improves some doc/comments

--- deploy/README.md
+++ deploy/README.md
@@ -1,22 +1,22 @@
 # Deploying the Website
 
-This script will fetch the latest version from the remote repo, 
+This script will fetch the latest version from the remote repo,
 build the site locally, and rsync that into the web root.
 
 ## Deploying:
 
-_Note:_ If this is your first time, see **Initial Setup** below. 
+_Note:_ If this is your first time, see **Initial Setup** below.
 
-Pull the latest version from github, activate the virtualenv, and run the script:
+Log into the webserver, pull the latest version from github, activate the virtualenv, and run the script:
 
     cd ~/ringsnode_website
     git pull
     cd deploy/
-    source venv/bin/activate 
+    source venv/bin/activate
     fab deploy
 
 
-## Initial Setup: 
+## Initial Setup:
 
 1. While logged into the web server, grab a local copy of the remote repo
 
@@ -32,11 +32,11 @@ Pull the latest version from github, activate the virtualenv, and run the script
 3. Activate the virtualenv and install the requirements
 
         source venv/bin/activate
-        pip install -r requirements.txt 
+        pip install -r requirements.txt
 
 4. Create a secrets.py file
 
-        cp secrets_template.py secrets.py 
+        cp secrets_template.py secrets.py
 
 
 5. Edit the secrets.py file
@@ -44,8 +44,6 @@ Pull the latest version from github, activate the virtualenv, and run the script
     Edit the secrets.py to define the web root directory.
     (Web root must be accessible locally to this script)
 
-6. Try it out! 
-    
+6. Try it out!
+
     fab deploy
-
-
--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -12,26 +12,24 @@ PROD_USR = getpass.getuser()
 REPO = 'https://github.com/basilleaf/ringsnode_website.git'
 
 def deploy():
-    """ This script will deploy the website to production, 
-        pulling it from the github repo. 
+    """ This script will deploy the website to production,
+        pulling it from the github repo.
         You must have sudo access to the production web root.
     """
 
     # You are running this in /deploy so go a level up and git pull the latest repo
     with lcd("../"):
         print("\nPulling the latest site from github.com: \n-----> %s\n" % REPO)
-        time.sleep(2.5)  # please remind people we are pulling from remote 
+        time.sleep(2.5)  # please remind people we are pulling from remote
         local("git pull")
 
-    # build the website. 
-    # Note that _config.yml must not be excluding any files!!!! 
-    # TODO: make this impossible to fail
+    # build the website.
     with lcd("../website"):
-        
+
         local("jekyll build --config _config.yml,_config.production.yml")
 
         # copy the website to the production directory
-        rsync_cmd = "sudo rsync -r %s --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ %s. " 
+        rsync_cmd = "sudo rsync -r %s --exclude=*.tif --exclude=*.tiff --exclude=*.tgz --exclude=*.tar.gz _site/ %s. "
 
         # first do a dry run:
         local(rsync_cmd % ('--dry-run --itemize-changes ',PROD_DIR))
@@ -41,4 +39,3 @@ def deploy():
             sys.exit()
         else:
             print("\nDeployment Aborted\n")
-
