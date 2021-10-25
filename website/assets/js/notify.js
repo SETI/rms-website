61a16fb1d84c7a04801b40b1e3d5dd7519e3e287
Debby Stopp <stoppmeister@gmail.com>
2021-10-25 12:04:35 -0700

    Update to add modal code, still commented out

---
+++ website/assets/js/notify.js
@@ -0,0 +1,26 @@
+$("#pds-notification-modal").on("click", ".btn", function() {
+    let target = $(this).data("target");
+    if ($(this).attr("type") == "submit") {
+        $.cookie("notify", $(`#${target}`).data("cookie"), {expires: 1000000});
+    }
+    $(`#${target}`).modal("hide");
+});
+
+$( document ).ready(function() {
+    $.get( "/important_message.html", function( data, status, xhr ) {
+        //console.log( xhr.getResponseHeader("Last-Modified") );
+        // if the lastMod stored in the cookie is different than the
+        // current last mod of the file, display the message
+        let cookie = xhr.getResponseHeader("Last-Modified");
+        if ($.cookie("notify") != cookie) {
+            //$("#pds-notification-modal .modal-body").html(data);
+            //$("#pds-notification-modal").modal("show");
+            //$("#pds-notification-modal").data("cookie", cookie);
+            $(".notification").html(data);
+        }
+    })
+    .fail(function() {
+        // and just null out the cookie if the file is gone...
+        $.cookie("notify", null);
+    });
+});
