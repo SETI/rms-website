58a6ed720b85709be12381538ccb84dcc9047e85
PDS Administrator <pds-admin@seti.org>
2022-05-18 17:55:48 -0700

    Update Linux deploy scripts

--- deploy/deploy_to_staging.sh
+++ deploy/deploy_to_staging.sh
@@ -5,14 +5,20 @@ cd ~/pds-website
 git checkout $branch
 git pull
 umask 002
+cd website
 jekyll build --config _config.yml,_config.production.yml
 echo '--- START DIFFS ---'
 diff -rq _site ${WEBROOT_DIR} | grep -v "Only in ${WEBROOT_DIR}" | grep -v "Common subdirectories"
 echo '--- END DIFFS ---'
 echo 'The above was a list of differences.'
 read -p 'If it looks good, push to staging site: [Y/n]' confirm
-if [[ $confirm -ne 'Y' ]] || [[ $confirm -ne '']]; then
+if [ x$confirm != 'xY' ] && [ x$confirm != 'x' ] && [ x$confirm != 'xy' ]
+then
   exit 0
+fi
 sudo rsync -av --checksum _site/ ${WEBROOT_DIR}
 sudo chown -R webmaster:www-data ${WEBROOT_DIR}
 sudo chmod -R g+w ${WEBROOT_DIR}
+echo "*** Staging website has been updated! ***"
+echo Take a look: ${STAGING_WEBSITE}
+
