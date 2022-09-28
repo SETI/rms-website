e9db16dc39fbf68e4e5a4fec49a555d0ad1a3c73
PDS Administrator <pds-admin@seti.org>
2022-09-27 17:44:32 -0700

    Update deploy scripts

--- deploy/deploy_website_to_staging.sh
+++ deploy/deploy_website_to_staging.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
-echo '### Deploying local pds-website repo to staging server'
 source config_data
-branch=${1:master}
+branch=${1:-master}
+echo "### Deploying pds-website repo, branch ${branch}, to staging server"
 cd ~/pds-website
 git checkout $branch
+if [ $? -ne 0 ]; then exit -1; fi
 git pull
+if [ $? -ne 0 ]; then exit -1; fi
 umask 002
 cd website
 jekyll build --config _config.yml,_config.production.yml
+if [ $? -ne 0 ]; then exit -1; fi
 echo '--- START DIFFS ---'
 diff -rq _site ${WEBROOT_DIR} | grep -v "Only in ${WEBROOT_DIR}" | grep -v "Common subdirectories"
 echo '--- END DIFFS ---'
@@ -17,8 +20,9 @@ if [ x$confirm != 'xY' ] && [ x$confirm != 'x' ] && [ x$confirm != 'xy' ]
 then
   exit 0
 fi
-sudo rsync -av --checksum _site/ ${WEBROOT_DIR}
-sudo chown -R webmaster:www-data ${WEBROOT_DIR}
-sudo chmod -R g+w ${WEBROOT_DIR}
+rsync -av --checksum _site/ ${WEBROOT_DIR}
+if [ $? -ne 0 ]; then exit -1; fi
+#sudo chown -R webmaster:www-data ${WEBROOT_DIR}
+#sudo chmod -R g+w ${WEBROOT_DIR}
 echo "*** Staging website has been updated! ***"
 echo Take a look: ${STAGING_WEBSITE}
