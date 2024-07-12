c069cad1e6b633c5fe59bf14028c351ada2bf492
Matthew Tiscareno <matt@seti.org>
2024-07-12 08:13:42 -0700

    Update repo name from pds-website to rms-website in deploy scripts (#205)

--- deploy/deploy_website_to_staging.sh
+++ deploy/deploy_website_to_staging.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 source config_data
 branch=${1:-main}
-echo "### Deploying pds-website repo, branch ${branch}, to staging server"
-cd ~/pds-website
+echo "### Deploying rms-website repo, branch ${branch}, to staging server"
+cd ~/rms-website
 git checkout $branch
 if [ $? -ne 0 ]; then exit -1; fi
 git pull
