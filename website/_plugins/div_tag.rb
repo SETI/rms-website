60e6d45b8cc2d6cacc6689c37b8c9e15759865b5
Debby Stopp <stoppmeister@gmail.com>
2026-02-26 12:27:19 -0800

    address code bot comments

--- website/_plugins/div_tag.rb
+++ website/_plugins/div_tag.rb
@@ -6,12 +6,12 @@ module Jekyll
     end
 
     def render(context)
-+      site = context.registers[:site]
-       content = super
--      converter = site.find_converter_instance(Jekyll::Converters::Markdown)
-+      rendered = converter.convert(content)
-+      "<div class=\"#{`@class`}\">#{rendered}</div>"    end
-  end
+-+    site = context.registers[:site]
+      content = super
+--    converter = site.find_converter_instance(Jekyll::Converters::Markdown)
++     rendered = converter.convert(content)
++     "<div class=\"#{`@class`}\">#{rendered}</div>"
++   end
 end
 
 Liquid::Template.register_tag('div_block', Jekyll::DivTag)
