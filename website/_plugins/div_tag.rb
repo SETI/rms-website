e3255d91f5d13db9e39df1978ba7882a5a608f0a
Debby Stopp <stoppmeister@gmail.com>
2026-04-07 12:39:53 -0700

    210 update galleries (#265)
    * Initial changes to support new gallery templates
    
    * Update the image-gallery template to accommodate the gallery pages.  Includes css changes
    
    Changes to the gallery page also include standardizing the landing page to the first page in the menu (either by date or name of target)
    Included are changes to accommodate the press release pages, but that is not currently being implemented.  That includes the press_release_data_table template, div_block.html template (which should be useful in other places) and sa few css changes.  In addition, there were some minor scss changes required for framework upgrade.
    
    * Revert "Update the image-gallery template to accommodate the gallery pages.  Includes css changes"
    
    This reverts commit 92dfd934b9697e5b27c495c75f98ff9d0e975196.
    
    * Reapply "Update the image-gallery template to accommodate the gallery pages.  Includes css changes"
    
    This reverts commit 0925e3bc8a355072c627e59bf100f80b04eaf35c.
    
    * These were deleted by a previous commit and need to stay deleted
    
    * change the nav to first/last
    
    makes more sense for targets
    
    * Addressed CodeRabbit comments
    
    * Minor fixes for codebot and searchbox
    
    * make the menu slightly bigger
    
    * remove unused class
    
    * Address code-bot comments
    
    * Address code bot comments
    
    * move inline style to .css file
    
    * address code bot comments
    
    * Address code-bot and accessibility issues
    
    * Address code bot comments
    
    * address code bot suggestions
    
    * Address codebot comments
    
    * Address codebot comment
    
    * change the container to flex per codebot suggestion

---
+++ website/_plugins/div_tag.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  class DivTag < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      super
+      @class = markup.strip
+    end
+
+    def render(context)
++      site = context.registers[:site]
+       content = super
+-      converter = site.find_converter_instance(Jekyll::Converters::Markdown)
++      rendered = converter.convert(content)
++      "<div class=\"#{`@class`}\">#{rendered}</div>"    end
+    end
+end
+
+Liquid::Template.register_tag('div_block', Jekyll::DivTag)
